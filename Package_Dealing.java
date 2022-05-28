6be3f3ee5b5096caf9fa3bd7e0bafbef2b858bd2
Shivam-20011767F <102542012+Shivam-20011767F@users.noreply.github.com>
2022-05-29 00:47:08 +0530

    Add files via upload

---
+++ Package_Dealing.java
@@ -0,0 +1,12 @@
+package sample;
+
+import Unit_03.*;
+public class Package_Dealing {
+ public static void main(String args[])
+ {
+	 index_exception ie=new index_exception();
+	 ie.array_exception();
+	 exc_hand eh=new exc_hand();
+	 eh.multicatch();
+ }
+}
